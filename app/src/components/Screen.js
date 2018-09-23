459f751b7947020155c47b539f31ca2ee0eab260
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-09-22 21:04:25 -0300

    :sparkles: creates Screen handlers for focus and blur

--- app/src/components/Screen.js
+++ app/src/components/Screen.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
+import React, { Component } from 'react';
+import { Dimensions, SafeAreaView } from 'react-native';
 
-const Screen = ({ children }) => (
-  <SafeAreaView>
-    { children }
-  </SafeAreaView>
-);
+const { height: SCREEN_SIZE } = Dimensions.get('window');
+
+class Screen extends Component {
+  call = (name) => (payload) => {
+    const λ = this.props[name];
+    return void (typeof λ === 'function' && λ(payload));
+  };
+
+  componentDidMount () {
+    const navigation = this.props.navigation;
+    if (!navigation)
+      return;
+    navigation.addListener('didBlur', this.call('onBlur'));
+    navigation.addListener('didFocus', this.call('onFocus'));
+    navigation.addListener('willBlur', this.call('onBeforeBlur'));
+    navigation.addListener('willFocus', this.call('onBeforeFocus'));
+  }
+
+  render () {
+    return (
+      <View style={ styles.container }>
+        <SafeAreaView>{ this.props.children }</SafeAreaView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: SCREEN_SIZE,
+  },
+});
+
+export { SCREEN_SIZE };
 
 export default Screen;

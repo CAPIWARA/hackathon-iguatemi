710527576d043641989845d2f547c1c175a4acb2
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-09-23 11:04:25 -0300

    improves Camera and send workflow

--- app/src/components/Screen.js
+++ app/src/components/Screen.js
@@ -5,7 +5,12 @@ import {
   SafeAreaView,
   StyleSheet,
   View,
+  Image,
+  TouchableOpacity
 } from 'react-native';
+import BackIcon from '../../assets/icons/Back.png';
+import BackDarkIcon from '../../assets/icons/Back-Dark.png';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 const { height: SCREEN_SIZE } = Dimensions.get('screen');
 
@@ -25,16 +30,32 @@ class Screen extends Component {
     navigation.addListener('willFocus', this.call('onBeforeFocus'));
   }
 
+  onMoveBackwards = () => this.props.navigation.goBack();
+
   render () {
     const bar = this.props.isDarkStatusBar ? 'light-content' : 'dark-content';
+    const icon = this.props.isDarkStatusBar ? BackIcon : BackDarkIcon;
 
     return (
       <View style={ styles.container }>
         <StatusBar barStyle={ bar }/>
-        <View>{  }</View>
         <SafeAreaView style={ styles.screen }>
           { this.props.children }
         </SafeAreaView>
+        {
+          this.props.isBackAllowed && (
+            <View style={ styles.header }>
+              <TouchableOpacity
+                style={ styles.arrow }
+                onPress={ this.onMoveBackwards }
+                activeOpacity={ 0.65 }
+              >
+                <Image source={ icon } />
+              </TouchableOpacity>
+              { this.props.header }
+            </View> 
+          )
+        }
       </View>
     );
   }
@@ -42,12 +63,32 @@ class Screen extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    position: 'relative',
     flex: 1,
     height: SCREEN_SIZE,
     backgroundColor: '#FFFFFF',
   },
 
   screen: { flex: 1 },
+
+  header: {
+    position: 'absolute',
+    top: getStatusBarHeight(),
+    left: 0,
+    right: 0,
+    width: '100%',
+    minHeight: 60,
+    alignItems: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  arrow: {
+    width: 40,
+    height: 27.76,
+  }
 });
 
 export { SCREEN_SIZE };
